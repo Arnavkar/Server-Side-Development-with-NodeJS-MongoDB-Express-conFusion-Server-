98482ea62cb27f1ba657f86aeb7629b735e7f1e9
Arnav Shirodkar <as9086@bard.edu>
2021-08-25 16:48:44 +0300

    Added facebook OAuth2 passport strategy

--- authenticate.js
+++ authenticate.js
@@ -6,6 +6,8 @@ const User = require('./models/users');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+
+const FacebookTokenStrategy = require('passport-facebook-token');
 const config = require('./config');
 
 //If we are not using passport-local-mongoose -> we must provide our own authentication function
@@ -49,3 +51,25 @@ exports.verifyAdmin = (req,res,next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy(
+    {
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    },(accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err,user) => {
+            if (err) return done(err,false);
+            if (!err && user !== null) return done(null,user);
+            else{
+                user = new User({username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err,user) => {
+                    if (err) return done(err, false);
+                    else return done(null,user);
+                });
+            }
+        });
+    }
+));
--- models/users.js
+++ models/users.js
@@ -16,6 +16,7 @@ var userSchema = new Schema({
     type:Boolean,
     default:false
   },
+  facebookID:String,
 },{
     timestamps: true
 });
